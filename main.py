c2cb78a1cd72a63b08370d8c27ac5a7b25691508
LordZeusIsBack <anubhavsharma5645@gmail.com>
2024-12-22 13:24:03 +0530

    Demographic Analysis Done

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from random import choice
 from faker.proxy import Faker
 from numpy.ma.core import arange
 from csv import writer
+import pandas as pd
+from pandas import DataFrame
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 def generate_data(num: int) -> None:
@@ -33,9 +37,44 @@ def generate_data(num: int) -> None:
 
     with open('random_data.csv', 'w', newline='', encoding='UTF-8') as fp:
         csv_writer = writer(fp)  # Create Writer Object
-        csv_writer.writerow(['ID', 'Name', 'Location', 'College', 'Year', 'Program', 'Source'])  # Header
+        csv_writer.writerow(['ID', 'Name', 'City', 'College', 'Year', 'Program', 'Source'])  # Header
         csv_writer.writerows(zip(ids, names_list, location_list, college_list, years_list, programs_list,
                                  sources_list))  # Writing the data into the csv file
+
+
+def demographic_analysis(df: DataFrame) -> None:
+    print('Demographic Analysis:')
+
+    # Trend analysis based on location
+    location_trend = df['City'].value_counts().head(5)  # Top 5 locations
+    print('\nLead sourcing based on location:')
+    print(location_trend)
+
+    # Trend analysis based on college
+    college_trend = df['College'].value_counts().head(5)  # Top 5 colleges
+    print('\nLead sourcing based on college:')
+    print(college_trend)
+
+    # Trend analysis based on year of study
+    year_trend = df['Year'].value_counts()
+    print('\nLead sourcing based on year of study:')
+    print(year_trend)
+
+    # Visualization
+    fig, axes = plt.subplots(3, 1, figsize=(12, 18))
+    sns.barplot(x=location_trend.index, y=location_trend.values, ax=axes[0])
+    axes[0].set_title('Lead Sourcing by Location')
+    axes[0].set_xticklabels(axes[0].get_xticklabels(), rotation=0)
+
+    sns.barplot(x=college_trend.index, y=college_trend.values, ax=axes[1])
+    axes[1].set_title('Lead Sourcing by College')
+    axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=0)
+
+    sns.barplot(x=year_trend.index, y=year_trend.values, ax=axes[2])
+    axes[2].set_title('Lead Sourcing by Year of Study')
+
+    plt.tight_layout()
+    plt.show()
 
 
 def main() -> None:
@@ -43,6 +82,12 @@ def main() -> None:
 
     # Generate fake data
     generate_data(size)
+
+    # Load the generated dataset
+    data = pd.read_csv('random_data.csv')
+
+    # Execute the tasks
+    demographic_analysis(data)
 
 
 if __name__ == '__main__':
